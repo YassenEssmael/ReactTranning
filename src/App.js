ac4c4fb5ad882aad15555fd8e62880f8239273f7
Yassen Essmael <yassenessmael@gmail.com>
2022-06-17 14:08:09 +0300

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import logo from './logo.svg';
 import { Container, Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 function App() {
+          
   return (
+    
     <div>
      <Navbar dark color='primary'>
        <div className="container">
          <NavbarBrand href='/'>Restorant con fusion </NavbarBrand>
        </div>
      </Navbar>
-     <Menu />
+     <Menu dishes={DISHES} />
 
     </div>
   );
